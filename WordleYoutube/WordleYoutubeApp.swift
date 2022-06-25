495a2ad28ca1b05e430dd5f13a90487e5ca8e15d
Necmettin Solmaz <necmettinsolmaz@icloud.com>
2022-06-25 23:30:20 +0300

    Initial Commit

---
+++ WordleYoutube/WordleYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleYoutubeApp.swift
+//  WordleYoutube
+//
+//  Created by Necmettin SOLMAZ on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordleYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
