393b3eecbd3e68ffafb2f9f23782d9dfdc5f81e3
necmettins <necmettinsolmaz@icloud.com>
2022-07-04 23:32:55 +0300

    Tahmin edilecek 6 kelime için kartlar ekranda gösterildi.

--- WordleYoutube/ContentView.swift
+++ WordleYoutube/ContentView.swift
@@ -10,46 +10,18 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var wordleVM = WordleViewModel()
     var body: some View {
-        NavigationView{
-            VStack(spacing: 3){
-                ForEach(wordleVM.kelimeler){ kelime in
-                    CardView(kelime: kelime)
+        VStack{
+            ForEach(wordleVM.kelimeler){ kelime in
+                HStack{
+                    ForEach(0..<5){ index in
+                        Text(kelime.harfler[index])
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                    }
                 }
-                Spacer()
-                KlavyeView()
-            } .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button {
-                            
-                        } label: {
-                            Image(systemName: "questionmark.circle")
-                        }
-                    }
-                    ToolbarItem(placement: .principal) {
-                        Text("WORDLE")
-                            .font(.largeTitle)
-                            .fontWeight(.heavy)
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        HStack{
-                            Button {
-                                
-                            } label: {
-                                Image(systemName: "chart.bar")
-                            }
-                            Button {
-                                
-                            } label: {
-                                Image(systemName: "gearshape.fill")
-                            }
-
-                        }
-                    }
-            
-                }
+            }
+            Spacer()
         }
-        
     }
 }
 
@@ -59,17 +31,4 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct CardView: View {
-    var kelime : WordleModel.Kelime
-    var body: some View {
-        HStack(spacing: 3){
-            ForEach(0..<5){ index in
-                Text(kelime.harfler[index])
-                    .frame(width: 60, height: 60)
-                    .background(Color.gray)
-                    .font(.title)
-                    .foregroundColor(.primary)
-            }
-        }
-    }
-}
+
